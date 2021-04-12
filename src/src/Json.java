505fe2452e19a396d52835aed8a3199aba2ecf04
Guanming Ou <u7130469@anu.edu.au>
2021-04-12 16:40:08 +1000

    added lab code as a sample for later development

---
+++ src/src/Json.java
@@ -0,0 +1,51 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Json {
+//    /**
+//     * Saves this collection to the specified JSON file.
+//     *
+//     * @param file The path to a file.
+//     */
+//    public void saveToJSONFile(File file) {
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//
+//        try(FileWriter fw = new FileWriter(file)){
+//            gson.toJson(books, fw);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//    /**
+//     * Load a pre-existing book collection from a JSON file.
+//     *
+//     * @param file The file to load from. This is guaranteed to exist.
+//     * @return An initialised book collection.
+//     */
+//    public static BookCollection loadFromJSONFile(File file) {
+//        Gson gson = new Gson();
+//        JsonReader jsonReader = null;
+//
+//        final Type CUS_LIST_TYPE = new TypeToken<List<Book>>() {}.getType();
+//        //or TypeToken.getParameterized(ArrayList.class, PersonJSON.class).getType();
+//
+//        try{
+//            jsonReader = new JsonReader(new FileReader(file));
+//        }catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        BookCollection bc = new BookCollection(gson.fromJson(jsonReader, CUS_LIST_TYPE));
+//        return bc;
+//    }
+}
--- src/src/UnitTests/GameTest.java
+++ src/src/UnitTests/GameTest.java
@@ -1,4 +1,20 @@
 package UnitTests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class GameTest {
+//    @Before
+//    public void creategame({
+//
+//    })
+
+    @Test(timeout=1000)
+    public void testJson() throws Exception {
+    }
 }
