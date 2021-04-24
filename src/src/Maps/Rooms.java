efe0faa6c13a508d075575e84d019a8630ae8a52
Guanming Ou <u7130469@anu.edu.au>
2021-04-24 18:23:42 +1000

    update

--- src/src/Maps/Rooms.java
+++ src/src/Maps/Rooms.java
@@ -2,6 +2,13 @@ package Maps;
 
 import NPCs.NPC;
 
+/**
+ * Basically, player are given option to go which room out of three options
+ * Initially the rooms are randommly draw with three option.
+ * Player can select which room they go to, either enter combat, shops, found a threasure, or receive blessing from fairies
+ * New three options are draw everytime player exit a room
+ * while previous options are droped back to the pool, and previous entered room is removed from the pool
+ */
 public class Rooms {
     String name;
     String description;
@@ -19,6 +26,5 @@ public class Rooms {
     public String getDescription() { return description; }
 
     public void setDescription(String description) { this.description = description; }
-
 
 }
