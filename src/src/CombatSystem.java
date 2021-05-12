c46965f4467ed66097bca522352dcdc3e5cb63d1
Yixiang Yin <u7112271@anu.edu.au>
2021-05-13 08:47:39 +1000

    added skeleton code in 'Game' and 'CombatSystem'

---
+++ src/src/CombatSystem.java
@@ -0,0 +1,12 @@
+public class CombatSystem {
+    // enemy's deck
+    // player's deck
+    // enemy
+    // player
+    public static void initializeCombatSystem(){
+        // initialize player's card
+
+        // initialize the enemy
+        // initialize enemy's card
+    }
+}
--- src/src/Deck.java
+++ src/src/Deck.java
@@ -4,6 +4,9 @@ import java.util.Random;
 public class Deck {
 public static class PlayerDeck extends ArrayList<Integer> {
 
+
+
+
     /**
      * Create player Deck for all current possible card that the player can play
      *
