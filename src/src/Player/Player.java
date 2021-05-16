468d468470bd2311c95f99fe779f5ad6c07c23e2
Guanming Ou <u7130469@anu.edu.au>
2021-05-16 22:19:56 +1000

    updated placement

--- src/src/Player/Player.java
+++ src/src/Player/Player.java
@@ -102,6 +102,40 @@ public class Player {
     }
 
     /**
+     * Save player as json
+     */
+    // TODO: This method is not tested yet, check and complete if needed - Guanming
+    public void save() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try(FileWriter fw = new FileWriter(this.name)){ // name json file with player's name
+            gson.toJson(this, fw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * load player from json
+     * @param playerName the name of specific player
+     */
+    // TODO: This method is not tested yet, check and complete if needed - Guanming
+    public static Player load(String playerName) {
+        File file = new File("json_files/"+ playerName +".json");
+
+        Gson gson = new Gson();
+        JsonReader jsonReader = null;
+
+        final Type CUS_LIST_TYPE = new TypeToken<Player>() {}.getType();
+
+        try{
+            jsonReader = new JsonReader(new FileReader(file));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return gson.fromJson(jsonReader, CUS_LIST_TYPE);
+    }
+
+    /**
     Consume an consumable item
      */
     public String consume(Item i){
@@ -134,40 +168,6 @@ public class Player {
                 return itemName+" is not consumable.";
             }
         }
-    }
-
-    /**
-     * Save player as json
-     */
-    // TODO: This method is not tested yet, check and complete if needed - Guanming
-    public void save() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-        try(FileWriter fw = new FileWriter(this.name)){ // name json file with player's name
-            gson.toJson(this, fw);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    /**
-     * load player from json
-     * @param playerName the name of specific player
-     */
-    // TODO: This method is not tested yet, check and complete if needed - Guanming
-    public static Player load(String playerName) {
-        File file = new File("json_files/"+ playerName +".json");
-
-        Gson gson = new Gson();
-        JsonReader jsonReader = null;
-
-        final Type CUS_LIST_TYPE = new TypeToken<Player>() {}.getType();
-
-        try{
-            jsonReader = new JsonReader(new FileReader(file));
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return gson.fromJson(jsonReader, CUS_LIST_TYPE);
     }
 
     /**
