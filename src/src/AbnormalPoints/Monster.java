a5a3a0cb1d2f3f1db6662c4f1bf57d7f36ae421f
Yitao Chen <u7004563@anu.edu.au>
2021-05-17 11:26:25 +0800

    MonsterGenerator
    Signed-off-by: Yitao Chen <u7004563@anu.edu.au>

--- src/src/AbnormalPoints/Monster.java
+++ src/src/AbnormalPoints/Monster.java
@@ -25,36 +25,4 @@ public class Monster extends AbnormalPoint {
         this.setGold(ma.setBaseGold + playerLevel * ma.setByLevelGold);
         this.setElement(ma.setElement);
     }
-
-    /**
-     * A monster with high health and damage, but low armour.
-     */
-    MonsterAttributes giant = new MonsterAttributes("giant","A monster with high health and damage, but low armour.",
-            150, 8, 6, 3,40,3,
-            0.03,50, 3,15, 11, Element.Normal);
-
-    /**
-     * A normal monster , with slight armour.
-     */
-    MonsterAttributes goblin = new MonsterAttributes("goblin", "A normal monster , with slight armour.",
-            55,6, 0,3,12,2,
-            0.02,10, 3,0,5, Element.Normal);
-
-    /**
-     * A quite weak monster.
-     */
-    MonsterAttributes skeleton = new MonsterAttributes("skeleton", "A quite weak monster.",
-            50,3, 0, 1, 8,1,
-            0.02,10, 3, 0,3,Element.Normal);
-
-    /**
-     * A monster without low damage, but high health and armour.
-     */
-    MonsterAttributes troll = new MonsterAttributes("troll", "A monster without low damage, but high health and armour.",
-            70,11, 0,12,20,3,
-            0.05,75, 3,25,10,Element.Normal);
-
-    MonsterAttributes wolf = new MonsterAttributes("wolf", "A wolf as you see",
-            35,3, 0,0,15,2,
-            0.04,25, 3,0,2,Element.Normal);
 }
