4c07bd66e762dbfdea5555ae8f73c8ca86a78e2d
Yitao Chen <u7004563@anu.edu.au>
2021-05-20 19:52:54 +0800

    basic auto test function
    Signed-off-by: Yitao Chen <u7004563@anu.edu.au>

--- src/src/autoTest.java
+++ src/src/autoTest.java
@@ -1,4 +1,3 @@
-import AbnormalPoints.AbnormalPoint;
 import CommandParser.CommandTokenizer;
 import CommandParser.Parser;
 import CommandParser.Token;
@@ -6,17 +5,11 @@ import Options.BasicOption;
 import Options.Control;
 import Options.PlayerOption;
 import Options.StartOption;
-import Player.Bag;
 import Player.Player;
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-import navigation.Coordinate;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -28,78 +21,34 @@ import java.util.Scanner;
 public class autoTest {
     private static Player player;
     public static CommandTokenizer cmdTok;
+    private static Random random;
     public Parser parser;
     //private List<Item> itemBook; // contain all possible items in the game(used for reference purposes)
 
-    /**
-     * initial pre-game
-     */
-    /*
-    public void initialize() {
-        itemBook = Item.initializeItemBook();
-    }
-
-/*    Test
-        public static void loaditem() {
-        File item = new File("json_files/Item2.json");
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Item itemNew = createItem();
-        String json = gson.toJson(itemNew);
-
-        try (FileWriter writer = new FileWriter("json_files/Item2.json")) {
-            gson.toJson(itemNew,writer);
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Item createItem() {
-        HashMap<String,Integer> map = new HashMap();
-        map.put("lala",12);
-        Item item = new Item("c1", "test1", "fire", "lalala", map);
-        return item;
-    }*/
-
-
-
-    /**
-     * Is this our main game?
-     *
-     * @param args
-     */
-
-    public static void main(String[] args) throws Exception {
+    public static void autoCaseTest() throws Exception {
         Game g = new Game();
         g.initialize();
-        Scanner s=new Scanner(System.in);
-        System.out.print("Welcome to the world!"+"\n");
-        System.out.println("What would you like to do?");
+
         StartOption start = new StartOption();
-        start.printOut();
+
         while(true) {
-            player = new Player("");
-            int i = start.getInput(s.next());
+            player = new Player("test player");
+            int i = random.nextInt(3)+1;
             switch (i){
                 case 0:
                     Control.death=false;
-                    System.out.println("what is your name?");
-                    String name = s.next();
-                    player.setName(name);
                     Control c = new Control(player);
                     gameInteractionLoop(player, c);
-//                gameInteractionLoopParser(player);
                     break;
                 case 1:
-                    System.out.println("Here is all the players available");
+                    //System.out.println("Here is all the players available");
                     PlayerOption playerOption = new PlayerOption(player);
                     playerOption.printOut();
                     boolean b = true;
                     while (b) {
-                        if(playerOption.chooseOp(new Token(s.next(),null))) {
+                        if(playerOption.chooseOp(new Token("test player",null))) {
                             player = playerOption.player;
                             b = false;
-                        } else {
-                            System.out.println("I don't understand this command");
                         }
                     }
                     Control.death=false;
@@ -109,11 +58,10 @@ public class autoTest {
                 case 2:
                     System.exit(0);
                 default:
-                    System.out.println("I don't understand this command");
             }
         }
-    }
 
+    }
 
     /**
      * This continuously ask for player's input to keep the game running,
@@ -121,22 +69,25 @@ public class autoTest {
      * @param player
      */
     public static void gameInteractionLoop(Player player, Control c) throws Exception {
-        System.out.println(player.checkMonster());
         boolean continueOn = true;
-        Scanner s = new Scanner(System.in);
+        List<String> commandList = new ArrayList<String>();
+        commandList.add("go north");
+        commandList.add("go south");
+        commandList.add("go west");
+        commandList.add("go east");
+        commandList.add("attack");
+
+        int length = commandList.size();
+
         while (continueOn){
             if (Control.death)
                 return;
             BasicOption current = c.currentOption;
-            cmdTok = new CommandTokenizer(s.nextLine());
+
+            cmdTok = new CommandTokenizer(commandList.get(random.nextInt(length)+1));
             player.checkNPCs();
             if (current.chooseOp(cmdTok.current())) {
                 cmdTok = current.convert(cmdTok);
-//                if (cmdTok.current()!=null && cmdTok.current().type()== Token.Type.DIRECTION) {
-//                    autoDetect();
-//                    c.printRightOption();
-//                }
-//                System.out.println(cmdTok.current().token());
                 continueOn = new Parser(cmdTok, player).parseCommand();
 //                System.out.println(cmdTok.current().token());
 //                if (cmdTok.current()!=null && cmdTok.current().type()== Token.Type.DIRECTION) { //TODO: no very good
@@ -162,5 +113,12 @@ public class autoTest {
             cmdTok = new CommandTokenizer(s.nextLine());
             continueOn = new Parser(cmdTok, player).parseCommand();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        long startTime=System.currentTimeMillis();
+        autoCaseTest();
+        long endTime=System.currentTimeMillis();
+        System.out.println(endTime-startTime);
     }
 }
